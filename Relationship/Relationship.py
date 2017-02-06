2dfc496cc2177657c1ba060ffa9cba098c3583f1
nathantfrank <nathan.t.frank@gmail.com>
2017-02-05 20:47:58 -0600

    Initial commit on Relationship class

---
+++ Relationship/Relationship.py
@@ -0,0 +1,15 @@
+class Relationship:
+
+    def __init__(self):
+        self.type = 'Relationship'
+        self.sensorX = 0
+        self.sensorY = 0
+
+    def onNewValue(self, value):
+        return
+
+    def getCorrelatoinCoefficient(self):
+        return
+
+    def setSensors(self, sensor1, sensor2):
+        return
