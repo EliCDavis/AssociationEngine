9b5fae5e7c3c2e3e36d4d9f042fdce740eb31b8e
nathantfrank <nathan.t.frank@gmail.com>
2017-02-09 16:30:23 -0600

    Variable update

--- Relationship/Variable.py
+++ Relationship/Variable.py
@@ -1,12 +1,23 @@
+from uuid import uuid4
+
+
 class Variable:
 
     def __init__(self):
         self.__class__ == 'Variable'
-        self.lastValue = 0.0
-        self.subscribers = []
+        self.subscribers = {}
+        self.uuid = uuid4()
 
-    def onData(self, snapshot):
-        return
+    def on_data(self, snapshot):
+        for subscriber in self.subscribers:
+            subscriber.on_new_value(snapshot, self.uuid)
 
-    def display(self, sensor, value):
-        return
+    def get_uuid(self):
+        return self.uuid
+
+    def add_subscriber(self, relationship):
+        self.subscribers[relationship.get_uuid()] = relationship
+
+    def remove_subscriber(self, relationship_uuid):
+        self.subscribers.pop(relationship_uuid, None)
+
