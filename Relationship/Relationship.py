3eb8f345373f625eb7391471222280f52a4ea76d
nathantfrank <nathan.t.frank@gmail.com>
2017-02-09 16:16:24 -0600

    Relationship update

--- Relationship/Relationship.py
+++ Relationship/Relationship.py
@@ -1,15 +1,12 @@
 class Relationship:
 
-    def __init__(self):
+    def __init__(self, sensor_x, sensor_y):
         self.type = 'Relationship'
-        self.sensorX = 0
-        self.sensorY = 0
+        self.sensor_x = sensor_x
+        self.sensor_y = sensor_y
 
-    def onNewValue(self, value):
+    def on_new_value(self, value, id_of_var):
         return
 
-    def getCorrelatoinCoefficient(self):
-        return
-
-    def setSensors(self, sensor1, sensor2):
+    def get_correlation_coefficient(self):
         return
