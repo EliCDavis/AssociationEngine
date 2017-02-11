ce1b86f418f26d05ddecfc25e1d52b32bf5274f8
RickArendall <TheCrimson02>
2017-02-10 23:40:58 -0600

    Added skeleton for AssociationMatrix Class.

---
+++ Snapper/AssociationMatrix.py
@@ -0,0 +1,21 @@
+class AssociationMatrix():
+
+    def __init__(self):
+        """
+        Simple initialization function.
+
+        Note that relationships is a list of lists of relationship objects.
+        """
+        self.relationships = []
+
+    def addRelationship(self, relationship):
+        pass
+
+    def removeRelationship(self, relationship):
+        pass
+
+    def onNewAssociationValue(self, value):
+        pass
+
+    def display(self, sensor, value):
+        pass
