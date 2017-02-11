5faaba55ff07dbaa202d0a4b3bea85d4d8aa6620
RickArendall <TheCrimson02>
2017-02-11 00:24:30 -0600

    Added uuid to Sensor.

--- Sensor/Sensor.py
+++ Sensor/Sensor.py
@@ -1,7 +1,11 @@
+from uuid import uuid4
+
+
 class Sensor():
 
     def __init__(self, snapper):
         self.snapper = snapper
+        self.uuid = uuid4()
     
     def Publish(self, data):
         self.snapper.onData(self, data)
