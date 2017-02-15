064ddca6859c7cc2b32630243da6125953e0e6d9
Eli Davis <eli.davis1995@gmail.com>
2017-02-15 00:21:13 -0500

    Please Work Travis

---
+++ README.md
@@ -0,0 +1,2 @@
+# Association Engine
+Engine for determing how related two variables are over a period of time.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+README = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+
+setup(
+    name = "association_engine",
+    version = "0.1.0",
+    packages=find_packages(),
+    author = "Senior Design Team MSU",
+    author_email = "ecd157@msstate.edu",
+    description = ("Correlation values between variables"),
+    long_description=open(README).read(),
+    license = "MIT",
+    keywords = "association correlation spearman",
+    url = "https://github.com/EliCDavis/AssociationEngine",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
