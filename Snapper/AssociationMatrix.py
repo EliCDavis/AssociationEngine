941e395d804635ead728ce0c69156974b9c1d59c
RickArendall <rickarendall@gmail.com>
2017-02-18 18:48:24 -0600

    Snapper Pull Request (#26)
    * Added skeleton for Snapper class.
    
    * Added pass lines to Snapper.py
    
    * Added skeleton for AssociationMatrix Class.
    
    * Added uuid to Sensor.
    
    * Added implementation to forwardSnapshot method of Snapper.
    
    * Adjusted Snapper and AssociationMatrix to more closely follow style guide.
    
    * Implemented basic passthrough functionality for Snapper.
    
    * Added tests for Snapper and AssociationMatrix classes.
    
    * Fixed test buildability by changing import statement, adding __init__.py to root, and changing method of checking for empty list/dictionary.
    
    * Fixed missing 'self.' on line 40 of Snapper.py and fixed style issues across branch.

---
+++ Snapper/AssociationMatrix.py
@@ -0,0 +1,21 @@
+class AssociationMatrix:
+
+    def __init__(self):
+        """
+        Simple initialization function.
+
+        Note that relationships is a list of lists of relationship objects.
+        """
+        self.relationships = {}
+
+    def add_relationship(self, relationship):
+        pass
+
+    def remove_relationship(self, relationship):
+        pass
+
+    def on_new_association_value(self, value):
+        pass
+
+    def display(self, sensor, value):
+        pass
