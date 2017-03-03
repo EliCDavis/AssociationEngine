3959b6fdf612562351b91ec431c7b0320a6de0ed
RickArendall <rickarendall@gmail.com>
2017-03-02 23:02:45 -0600

    Added tests to verify initialization of snapper and matrix in Manager

--- Tests/Manager_test.py
+++ Tests/Manager_test.py
@@ -1,6 +1,13 @@
 from Snapper.Manager import Manager
+from Snapper.Snapper import Snapper
+from Snapper.AssociationMatrix import AssociationMatrix
 
 
 def test_should_initialize_object():
     manager = Manager()
     assert isinstance(manager, Manager)
+
+def test_should_initialize_snapper_and_matrix():
+    manager = Manager()
+    assert isinstance(manager.snapper, Snapper)
+    assert isinstance(manager.matrix, AssociationMatrix)
