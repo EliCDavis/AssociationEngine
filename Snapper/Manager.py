a395173ac498c8ce49730e50659dfd14a4f46797
RickArendall <rickarendall@gmail.com>
2017-03-03 00:10:56 -0600

    Added basic sensor addition and deletion to Manager

--- Snapper/Manager.py
+++ Snapper/Manager.py
@@ -1,31 +1,37 @@
 from Snapper.Snapper import Snapper
 from Snapper.AssociationMatrix import AssociationMatrix
+from Relationship.Variable import Variable
 
 
 class Manager:
 
     def __init__(self):
         self.sensors = []
-        self.dataBuffer = {}
-        self.routeMap = {}
+        self.route_map = {}
         self.variables = []
         self.snapper = Snapper()
         self.matrix = AssociationMatrix()
 
-    def add_sensor(self):
+    def add_sensor(self, sensor):
         """
         This function adds a new sensor to the snapper module and generates
-        a corresponding variable object.
+        a corresponding variable object with mappings.
 
         :return:
         """
         self.sensors.append(sensor)
+        self.snapper.add_sensor(sensor)
         newVariable = Variable()
         self.variables.append(newVariable)
-        self.routeMap[sensor.uuid] = newVariable.uuid
+        self.route_map[sensor.uuid] = newVariable
 
-    def remove_sensor(self):
-        pass
+    def remove_sensor(self, sensor):
+        variable = self.route_map[sensor.uuid]
+        self.route_map.pop(sensor.uuid)
+
+        self.snapper.remove_sensor(sensor)
+        self.sensors.remove(sensor)
+        self.variables.remove(variable)
 
     def get_matrix(self):
         pass
--- Tests/Manager_test.py
+++ Tests/Manager_test.py
@@ -1,6 +1,8 @@
 from Snapper.Manager import Manager
 from Snapper.Snapper import Snapper
 from Snapper.AssociationMatrix import AssociationMatrix
+from Sensor.Sensor import Sensor
+from Relationship.Variable import Variable
 
 
 def test_should_initialize_object():
@@ -12,3 +14,17 @@ def test_should_initialize_snapper_and_matrix():
     manager = Manager()
     assert isinstance(manager.snapper, Snapper)
     assert isinstance(manager.matrix, AssociationMatrix)
+
+
+def test_add_and_remove_sensor_variable_pair():
+    manager = Manager()
+    sensor = Sensor(manager.snapper)
+    manager.add_sensor(sensor)
+
+    assert isinstance(manager.sensors[0], Sensor)
+    assert isinstance(manager.variables[0], Variable)
+
+    manager.remove_sensor(sensor)
+
+    assert len(manager.sensors) is 0
+    assert len(manager.variables) is 0
