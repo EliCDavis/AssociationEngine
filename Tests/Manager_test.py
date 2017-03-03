20ad65cc19e189c487639c3b35317a9c2350963e
RickArendall <rickarendall@gmail.com>
2017-03-02 23:43:39 -0600

    Pep8

--- Tests/Manager_test.py
+++ Tests/Manager_test.py
@@ -7,6 +7,7 @@ def test_should_initialize_object():
     manager = Manager()
     assert isinstance(manager, Manager)
 
+
 def test_should_initialize_snapper_and_matrix():
     manager = Manager()
     assert isinstance(manager.snapper, Snapper)
