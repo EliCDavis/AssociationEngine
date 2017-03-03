08be5f20ba4ef87090a12dfecc02269982122cb8
RickArendall <rickarendall@gmail.com>
2017-03-02 22:43:54 -0600

    Added skeleton for Manager class and its tests

---
+++ Snapper/Manager.py
@@ -0,0 +1,18 @@
+class Manager:
+
+    def __init__(self):
+        self.sensors = []
+        self.dataBuffer = {}
+        self.routeMap = {}
+        self.snapshot = {}
+        self.startTime = 0.0
+        self.variables = []
+
+    def add_sensor(self):
+        pass
+
+    def remove_sensor(self):
+        pass
+
+    def get_matrix(self):
+        pass
