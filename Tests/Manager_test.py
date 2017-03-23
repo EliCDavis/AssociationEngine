e3f6e402c015936f357f5cf658c1abf20600c026
RickArendall <rickarendall@gmail.com>
2017-03-22 23:49:26 -0500

    Added test covering multiple sensor addition and removal to Manager testing Suite and fixed issue with remove_sensor

--- Tests/Manager_test.py
+++ Tests/Manager_test.py
@@ -24,6 +24,23 @@ def test_add_and_remove_sensor_variable_pair():
     assert manager.sensors == []
     assert manager.variables == []
 
+def test_add_multiple_sensors():
+    manager = Manager()
+    sensor1 = Sensor()
+    sensor2 = Sensor()
+    manager.add_sensor(sensor1)
+    manager.add_sensor(sensor2)
+
+    assert manager.sensors == [sensor1, sensor2]
+    assert isinstance(manager.variables[0], Variable)
+    assert isinstance(manager.variables[1], Variable)
+
+    manager.remove_sensor(sensor1)
+    manager.remove_sensor(sensor2)
+
+    assert manager.sensors == []
+    assert manager.variables == []
+
 
 def test_return_matrix():
     manager = Manager()
