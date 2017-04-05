395b70278c5f494751b1516ee6a7f051b34da2a1
TylerNarmore <tyler@narmore.net>
2017-04-04 21:41:47 -0500

    Subscribers
    Adds Subscriber class and update relationships for socket

--- Examples/webpage/Server.py
+++ Examples/webpage/Server.py
@@ -7,6 +7,8 @@ from flask_socketio import SocketIO
 from Sensor.Sensor import Sensor
 from Snapper.Manager import Manager
 
+from Examples.webpage.Subscriber import Subscriber
+
 
 app = Flask(__name__, static_folder='dist', static_url_path='')
 io = SocketIO(app)
@@ -33,6 +35,12 @@ def client_connected():
     print("New Connection")
     for uuid in map(lambda sensor: sensor.uuid, AEManager.sensors):
         io.emit("sensor added", json.dumps(str(uuid)))
+
+
+def update_relationship(sensor_x,sensor_y,value):
+    io.emit('update relationship', {"sensor_x":sensor_x,
+                                    "sensor_y":sensor_y,
+                                    "value":value})
 
 
 def setup_Manager():
