3982603cbd8078c8a8fcd4899bf19124c4c29388
Eli Davis <eli.davis1995@gmail.com>
2017-04-20 15:14:34 -0500

    Working on making tests

--- AssociationEngine/Tests/Relationship_test.py
+++ AssociationEngine/Tests/Relationship_test.py
@@ -1,10 +1,35 @@
 from AssociationEngine.Relationship.Relationship import Relationship
 from AssociationEngine.Relationship.Variable import Variable
+import pytest
 
 
 def test_should_have_last_pushed_value_as_none_on_initialization():
     relationship = Relationship(Variable(), Variable())
     assert relationship.get_last_pushed_value() is None
+
+
+def test_should_should_throw_error_trying_to_grab_coefficient():
+    with pytest.raises(NotImplementedError) as excinfo:
+        Relationship(Variable(), Variable()).get_correlation_coefficient()
+    assert "Underlying algorithm should implement this" in str(excinfo.value)
+
+
+def test_should_should_throw_error_trying_get_new_value_from_variables():
+    with pytest.raises(NotImplementedError) as excinfo:
+        Relationship(Variable(), Variable()).on_new_value(1, 1)
+    assert "Underlying algorithm should implement this" in str(excinfo.value)
+
+
+def test_should_should_throw_error_trying_to_get_value_between_times():
+    with pytest.raises(NotImplementedError) as excinfo:
+        Relationship(Variable(), Variable()).get_value_between_times()
+    assert "Underlying algorithm should implement this" in str(excinfo.value)
+
+
+def test_should_should_throw_error_trying_clean_up():
+    with pytest.raises(NotImplementedError) as excinfo:
+        Relationship(Variable(), Variable()).clean_up()
+    assert "Underlying algorithm should implement this" in str(excinfo.value)
 
 
 def test_should_have_personal_uuid():
