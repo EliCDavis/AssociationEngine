ebcd3ef3f7bc5c5e6d5afff407fcdb39e45ac24a
nathantfrank <nathan.t.frank@gmail.com>
2017-05-01 22:12:50 -0500

    code coverage ensurance and updates to tests

--- AssociationEngine/Tests/SpearframeRelationship_test.py
+++ AssociationEngine/Tests/SpearframeRelationship_test.py
@@ -6,12 +6,23 @@ from AssociationEngine.Relationship.SpearframeRelationship import \
     SpearframeRelationship
 
 
-def test_should_initialize_empty_frames():
-    rel = SpearframeRelationship(Variable(), Variable())
-    assert len(rel.frames) == 0
+def test_should_show_perfect_association():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
+    var1 = Variable()
+    var2 = Variable()
+    rel = SpearframeRelationship(var1, var2)
+
+    for degree in range(180):
+        var1.on_data(math.sin(math.radians(degree * 10)), degree, degree+1)
+        var2.on_data(math.sin(math.radians(degree * 10)), degree, degree+1)
+
+    assert rel.get_last_pushed_value() == 1
 
 
 def test_should_show_strong_association():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -21,10 +32,12 @@ def test_should_show_strong_association():
         var2.on_data(math.cos(math.radians(degree * 10) + (math.pi / 2.0)),
                      degree, degree+1)
 
-    assert rel.get_last_pushed_value() > .95
+    assert rel.get_correlation_coefficient() > .95
 
 
 def test_should_show_weak_association():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -33,10 +46,12 @@ def test_should_show_weak_association():
         var1.on_data(math.sin(math.radians(degree * 10)), degree, degree+1)
         var2.on_data(math.cos(math.radians(degree * 50)), degree, degree+1)
 
-    assert rel.get_last_pushed_value() < .1
+    assert rel.get_correlation_coefficient() < .1
 
 
 def test_should_not_return_nan():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -45,7 +60,7 @@ def test_should_not_return_nan():
         var1.on_data(math.sin(math.radians(degree * 10)), degree, degree+1)
         var2.on_data(math.sin(math.radians(degree * 100)), degree, degree+1)
 
-    assert not math.isnan(rel.get_last_pushed_value())
+    assert not math.isnan(rel.get_correlation_coefficient())
 
 
 def test_should_create_db():
@@ -60,6 +75,8 @@ def test_should_create_db():
 
 
 def test_should_delete_db():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -71,6 +88,8 @@ def test_should_delete_db():
 
 
 def test_should_return_float():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -83,6 +102,8 @@ def test_should_return_float():
 
 
 def test_should_return_float2():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -96,6 +117,8 @@ def test_should_return_float2():
 
 
 def test_should_return_float3():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
     var1 = Variable()
     var2 = Variable()
     rel = SpearframeRelationship(var1, var2)
@@ -107,13 +130,15 @@ def test_should_return_float3():
     assert isinstance(rel.get_correlation_coefficient(), float)
 
 
-# def test_should_return_float_from_frame_db():
-#     var1 = Variable()
-#     var2 = Variable()
-#     rel = SpearframeRelationship(var1, var2)
-#
-#     for degree in range(180):
-#         var1.on_data(math.sin(math.radians(degree * 10)), degree, degree+1)
-#         var2.on_data(math.cos(math.radians(degree * 10)), degree, degree+1)
-#
-#     assert isinstance(rel.get_value_between_times(0, 90), float)
+def test_should_return_float_from_frame_db():
+    if os.path.exists("spearframe.db"):
+        os.remove("spearframe.db")
+    var1 = Variable()
+    var2 = Variable()
+    rel = SpearframeRelationship(var1, var2)
+
+    for degree in range(180):
+        var1.on_data(math.sin(math.radians(degree * 10)), degree, degree+1)
+        var2.on_data(math.cos(math.radians(degree * 10)), degree, degree+1)
+
+    assert isinstance(rel.get_value_between_times(0, 90), float)
